57f033b96ed9a5467873e800e79167bb77b319d4
Alexandre <naitwa.alexandre@gmail.com>
2021-05-02 03:57:16 -0300

    footer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const blocksWrap = document.getElementById('game-blocks');
 
 const addBlocksRow = () => {
   const row = 3;
+  
   for (let i = 0; i < row; i += 1) {
     const blockRow = document.createElement('div');
     blockRow.classList.add('blockRow');
@@ -13,13 +14,13 @@ addBlocksRow();
 const addBlocksCol = () => {
   const blocksRow = blocksWrap.childNodes;
   const col = blocksRow.length;
-  for (let i = 0; i < col; i += 1) {
-    for (let j = 0; j < col; j += 1) {
-      const blockCol = document.createElement('div');
-      blockCol.classList.add('blockCol');
-      blocksRow[i].appendChild(blockCol)
+
+  blocksRow.forEach((b) => {
+    for (let i = 0; i < col; i += 1) {
+      const block = document.createElement('div');
+      block.classList.add('blockCol');
+      b.appendChild(block);
     }
-  }
- 
+  });
 }
 addBlocksCol();
