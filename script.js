60c8180a96fd96ed23c9da0d105cc4735f594bcb
Alexandre <naitwa.alexandre@gmail.com>
2021-05-03 20:37:36 -0300

    faltando logica de win

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// global variables
 const blocksWrap = document.getElementById('game-blocks');
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
+const prop1 = window.getComputedStyle(player1, null)
+.getPropertyValue('background-color');
+const prop2 = window.getComputedStyle(player2, null)
+.getPropertyValue('background-color');
+
 
 // creates rows of blocks
 const addBlocksRow = () => {
@@ -32,18 +38,32 @@ const transitionMove = () => {
   const blocks = document.querySelectorAll('.blockCol');
   for(let i = 0; i < blocks.length; i += 1) {
     blocks[i].addEventListener('click', (e) => {
+      const blocks = e.target;
       player1.classList.toggle('turnOff');
       player2.classList.toggle('turnOff');
-      const blocks = e.target;
-      const prop1 = window.getComputedStyle(player1, null)
-      .getPropertyValue('background-color');
-      const prop2 = window.getComputedStyle(player2, null)
-      .getPropertyValue('background-color');
-      player2.classList.contains('turnOff') ? 
-        blocks.style.background = prop2 : blocks.style.background = prop1;
-      player2.classList.contains('turnOff') ? 
-        blocks.innerHTML = 'O' : blocks.innerHTML = 'X';
-    })
+
+      if (player2.classList.contains('turnOff')) {
+        blocks.classList.add('clickedO');
+        blocks.innerHTML = 'O'
+        blocks.style.background = prop2
+        blocks.style.pointerEvents = 'none';
+      } else {
+        blocks.classList.add('clickedX');
+        blocks.innerHTML = 'X';
+        blocks.style.background = prop1;
+        blocks.style.pointerEvents = 'none';
+      }
+    });
   }
 }
 transitionMove();
+
+// enumerate blocks
+const enumerateBlocks = () => {
+  const blocks = document.querySelectorAll('.blockCol');
+  for (let i = 0; i < blocks.length; i += 1) {
+    blocks[i].classList.add(`block${i}`);
+  }
+}
+enumerateBlocks();
+
