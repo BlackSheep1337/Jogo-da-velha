111478e9ec22c380399f2aa4a9ad567a8ad3719a
Alexandre <naitwa.alexandre@gmail.com>
2021-05-03 23:04:34 -0300

    implementar win logic

--- script.js
+++ script.js
@@ -33,11 +33,38 @@ const addBlocksCol = () => {
 }
 addBlocksCol();
 
+// win logic
+const logicWin = () => {
+  const blocks = document.querySelectorAll('.blockCol');
+  let winner = undefined;
+
+  const a1 = blocks[0].getAttribute('class');
+  const a2 = blocks[1].getAttribute('class');
+  const a3 = blocks[2].getAttribute('class');
+  
+  const b1 = blocks[3].getAttribute('class');
+  const b2 = blocks[4].getAttribute('class');
+  const b3 = blocks[5].getAttribute('class');
+  
+  const c1 = blocks[6].getAttribute('class');
+  const c2 = blocks[7].getAttribute('class');
+  const c3 = blocks[8].getAttribute('class');
+
+  if ((a1 === b1 && a1 === c1) || (a1 === a2 && a1 === a3) || (a1 === b2 && a1 === c3) && a1 !== '') {
+    
+  } else if ((b2 === b1 && b2 === b3) || (b2 === a2 && b2 === c2) || (b2 === a3 && b2 === c1) && b2 !== '') {
+    
+  } else if ((c3 === c2 && c3 === c1) || (c3 === a3 && c3 === b3) && c3 !== '') {
+    
+  } 
+
+}
+
 // transition moviment from one player to another.
 const transitionMove = () => {
   const blocks = document.querySelectorAll('.blockCol');
   for(let i = 0; i < blocks.length; i += 1) {
-    blocks[i].addEventListener('click', (e) => {
+    blocks[i].addEventListener('click',(e) => {
       const blocks = e.target;
       player1.classList.toggle('turnOff');
       player2.classList.toggle('turnOff');
@@ -53,17 +80,9 @@ const transitionMove = () => {
         blocks.style.background = prop1;
         blocks.style.pointerEvents = 'none';
       }
+      logicWin();
     });
+   
   }
 }
 transitionMove();
-
-// enumerate blocks
-const enumerateBlocks = () => {
-  const blocks = document.querySelectorAll('.blockCol');
-  for (let i = 0; i < blocks.length; i += 1) {
-    blocks[i].classList.add(`block${i}`);
-  }
-}
-enumerateBlocks();
-
